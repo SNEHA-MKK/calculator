7bbb2051b858f993abdb9ce3e045052dad2660eb
SNEHA-MKK <sneha.mohandas.kk@gmail.com>
2024-06-01 22:52:11 +0530

    updated

--- calc.js
+++ calc.js
@@ -9,21 +9,30 @@ function clearScreen(){
 
 function back(){
     screen.value=screen.value.slice(0,-1)
+    // slice - string method-as all inputs are given as string here
 }
+
 
 function findResult(){
 
+    // to resolve run time error(eg: if input is //66*/34)
+
+
     try{
+
     var result = eval(screen.value)
     screen.value=result;
+
     }
     
     catch{
         screen.value='error'
 
-        setTimeout(() => {                    // to change to 0 after showing error
+        setTimeout(() => {     
+            //change to 0 again( after 1 sec) after showing error
             screen.value=''
         }, 1000);
     }
 
 }
+//hw-try to develop scientific calculator from this
